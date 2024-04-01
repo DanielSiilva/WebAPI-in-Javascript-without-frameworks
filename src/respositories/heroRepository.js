a81a1da9f8d9dd57b9149f14cb244883a1ba631c
DanielSiilva <daniellsousa0614@gmail.com>
2024-04-01 08:52:30 -0300

    feat: heroRepository

--- src/respositories/heroRepository.js
+++ src/respositories/heroRepository.js
@@ -1,1 +1,29 @@
-console.log("Hello Word!");
+const { readFile, writeFile } = require("fs");
+
+class HeroRopository {
+  constructor({ file }) {
+    this.file = file;
+  }
+
+  async _currentFileContent() {
+    return JSON.parse(await readFile(this.file));
+  }
+
+  async find(itemId) {
+    const all = await this._currentFileContent();
+    if (!itemId) return all;
+
+    return all.find(({ id }) => itemId === id);
+  }
+
+  async create(data) {
+    const currentFile = await this._currentFileContent();
+    currentFile.push(data);
+
+    await writeFile(this.file, JSON.stringify(currentFile));
+
+    return data.id;
+  }
+}
+
+module.exports = HeroRopository;
