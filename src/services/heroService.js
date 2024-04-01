749162a2c0e89ac07098cf516a7e3cb22720030d
DanielSiilva <daniellsousa0614@gmail.com>
2024-04-01 08:55:19 -0300

    feat: services

--- src/services/heroService.js
+++ src/services/heroService.js
@@ -1,1 +1,15 @@
-console.log("Hello Word!");
+class HeroService {
+  constructor({ heroRepository }) {
+    this.heroRepository = heroRepository;
+  }
+
+  async find(itemId) {
+    return this.heroRepository.find(itemId);
+  }
+
+  async create(data) {
+    return this.heroRepository.create(data);
+  }
+}
+
+module.exports = HeroService;
